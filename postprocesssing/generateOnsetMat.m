cc5f5a91e006f76fa71c8f6b354915bfd8a6910b
Sushmita Senapati <sushmitaS16@users.noreply.github.com>
2024-05-14 16:11:49 +0200

    Update generateOnsetMat.m

--- postprocesssing/generateOnsetMat.m
+++ postprocesssing/generateOnsetMat.m
@@ -10,3 +10,5 @@ for chan=1:384
     py_nomenclature_chan = chan-1;
     onsetpoint{chan} = readNPY(fullfile(file_loc,strcat('spikeonsets_channel_',num2str(py_nomenclature_chan),'.npy')))';
 end
+
+save fullfile(file_loc,strcat('spikeonsetpoint')) onsetpoint
