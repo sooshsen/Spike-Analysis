9a15d4f89ce9b9c0dbccc9c5954e05e53ab5f913
Sushmita Senapati <sushmitaS16@users.noreply.github.com>
2024-05-14 14:58:31 +0200

    Update combineStats.py

--- preprocessing/combineStats.py
+++ preprocessing/combineStats.py
@@ -26,6 +26,7 @@ def combine_stats(all_mean, all_var, sampls, N):
     combined_mean = []
     combined_std = []
     
+    # initialization
     numerator_mean = 0
     total_sampl = 0
     net_q = 0
@@ -34,13 +35,13 @@ def combine_stats(all_mean, all_var, sampls, N):
         for exp in range(0,N):
             
             # mean
-            product_mean_sampl = sampls(channel,exp)*all_mean(channel,exp)
+            product_mean_sampl = sampls.iloc[channel,exp]*all_mean.iloc[channel,exp]
             
             numerator_mean = numerator_mean + product_mean_sampl
-            total_sampl = total_sampl + sampls(channel,exp)
+            total_sampl = total_sampl + sampls.iloc[channel,exp]
             
             # variance
-            q = (sampls(channel,exp) - 1)*all_var(channel,exp) + sampls(channel,exp)*(all_mean(channel,exp)**2)
+            q = (sampls.iloc[channel,exp] - 1)*all_var.iloc[channel,exp] + sampls.iloc[channel,exp]*(all_mean.iloc[channel,exp]**2)
             
             net_q = net_q + q;
         
@@ -52,14 +53,10 @@ def combine_stats(all_mean, all_var, sampls, N):
         # variance - stddev
         net_stddev_per_chan = math.sqrt((net_q - total_sampl*net_mean_per_chan**2)/(total_sampl-1))
         combined_std.append(net_stddev_per_chan)
-       
-        
-        
-    savehere = Path('D:/sushmitaS16/python_analysis/combined_limits_for_spikes')
-    stats = pd.DataFrame({'mean': combined_mean, 'standard_dev': combined_std})   
-    np.save(os.path.join(savehere, 'combined_stats'), stats)   # .npy file saved with the limits for the tuning curve data
 
-    return stats
+    savehere = Path().resolve()     # current directory
+    stats = pd.DataFrame({'mean': combined_mean, 'standard_dev': combined_std})   
+    np.save(os.path.join(savehere, 'combined_stats'), stats)  # combined mean and standard deviation from all experiments
     
   
 def main():
@@ -76,7 +73,6 @@ def main():
         mean_all = pd.concat([mean_all, file.iloc[:,0]], axis=1, ignore_index=True)
         var_all = pd.concat([var_all, file.iloc[:,1]], axis=1, ignore_index=True)
         num_sampl = pd.concat([num_sampl, file.iloc[:,2]], axis=1, ignore_index=True)
-        
         
     # call function
     combine_stats(mean_all, var_all, num_sampl, len(mean_all.columns))
