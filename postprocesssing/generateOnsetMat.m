706cb26774fcb83d9a2125845d18c843f952d716
Sushmita Senapati <sushmitaS16@users.noreply.github.com>
2024-05-14 15:28:55 +0200

    Create generateOnsetMat.m

---
+++ postprocesssing/generateOnsetMat.m
@@ -0,0 +1,12 @@
+% after spike onset detection in python
+% make matlab compatible file for individual experiments
+% @sushmitaS16
+clear;
+clc;
+
+file_loc = path/to/spikeonsets.npy;
+
+for chan=1:384
+    py_nomenclature_chan = chan-1;
+    onsetpoint{chan} = readNPY(fullfile(file_loc,strcat('spikeonsets_channel_',num2str(py_nomenclature_chan),'.npy')))';
+end
