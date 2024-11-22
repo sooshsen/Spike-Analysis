a947d692b5686dedd0d54e042b00df253991df4c
Sushmita Senapati <sushmitaS16@users.noreply.github.com>
2024-11-22 19:53:51 +0100

    Update getTriggers.py

--- preprocessing/getTriggers.py
+++ preprocessing/getTriggers.py
@@ -9,9 +9,18 @@ This script needs installation of open-ephys-python-tools module from Github
 Can be used for saving MU, SU, LFP related triggers
 Based on above, they are saved in different folders respectively
 
-NOTE: Next, to automate this extraction using a destinations.txt like file
+Note: This does not take a lot of time for computation, hence can be run in the local PC
+
 
 """
+
+def get_inputs():
+    
+    # OPEN TXT FILE FOR EXPERIMENT and save as numpy array
+    all_exps = np.loadtxt(Path("G:/Sushmita/spike_analysis/metafiles_MU_TC_only/benny_for_triggers.txt"), comments='#', dtype='str')
+    
+    return all_exps
+
 
 def name_folder(path):
     # create results folder based on original data file
@@ -29,7 +38,7 @@ def name_folder(path):
           
     folder = folder.lower()
     folder_name_loc = re.search(r".p[0-9].\w+.", folder) # for penetrations <10
-    # folder_name_loc = re.search(r".p[0-9]\w+.\w+.", folder) # for penetrations >=10
+    #folder_name_loc = re.search(r".p[0-9]\w+.\w+.", folder) # for penetrations >=10
     ind = folder_name_loc.span()
     folder = folder[ind[0]+1:ind[1]-1]
     folder = folder.replace('.', '_')
@@ -51,16 +60,18 @@ def trigger_onset(trigger, savehere):
     trigger_ind_for_matlab = trigger_onset_loc[0] + 2        # NOTE: In Matlab, index starts from 1 (not 0)
     
     # save the locations of the trigger onsets
-    np.save(os.path.join(savehere, 'trigger_onset_for_py'), trigger_ind_for_py)
-    np.save(os.path.join(savehere, 'trigger_onset_for_matlab'), trigger_ind_for_matlab)
+    np.save(os.path.join(savehere, 'ss_trigger_onset_for_py'), trigger_ind_for_py)
+    np.savetxt(os.path.join(savehere, 'ss_trigger_onset_for_matlab.csv'), trigger_ind_for_matlab)
+    #np.save(os.path.join(savehere, 'trigger_onset_for_matlab'), trigger_ind_for_matlab)
+    print('Corresponding trigger onset locations saved...')
     
     
 
 
 def save_AP_trigger_MU(trigger, source):
     # save as npy
-    # dest_dir = '/home/ssenapat/groups/PrimNeu/Final_exps_spikes/MUA/Elfie/p1/'
-    dest_dir = Path('G:/Final_exps_spikes/MUA/Elfie/p2')
+    # dest_dir = '/home/ssenapat/groups/PrimNeu/Final_exps_spikes/MU_TC_only/Benny'
+    dest_dir = Path('G:/Final_exps_spikes/MU_TC_only/Benny')
     foldername = name_folder(source)
     
     savehere = Path(os.path.join(dest_dir, foldername))
@@ -68,7 +79,10 @@ def save_AP_trigger_MU(trigger, source):
     if not os.path.exists(savehere):     # if the required file does not exist, create one
         os.mkdir(savehere)
     
-    np.save(os.path.join(savehere, 'trigger_ap'), trigger)
+    np.save(os.path.join(savehere, 'ss_trigger_ap'), trigger)
+    np.savetxt(os.path.join(savehere, 'ss_trigger_ap.csv'), trigger)
+    print('\n')
+    print('Trigger channel amplitudes (AP) saved for multi-unit analysis')
     
     trigger_onset(trigger, savehere)
  
@@ -86,6 +100,8 @@ def save_AP_trigger_SU(trigger, source):
         os.mkdir(savehere)
     
     np.save(os.path.join(savehere, 'trigger_ap'), trigger)
+    print('\n')
+    print('Trigger channel amplitudes (AP) saved for single-unit analysis')
     
     trigger_onset(trigger, savehere)
 
@@ -103,54 +119,56 @@ def save_LFP_trigger(trigger, source):
         os.mkdir(savehere)
     
     np.save(os.path.join(savehere, 'trigger_lfp'), trigger)
+    print('\n')
+    print('Trigger channel amplitudes (LFP) saved')
     
     trigger_onset(trigger, savehere)
     
 
 
 
-def main():   
-    ## LOCAL
-    directory = Path('G:/Aryo/copy_data/Elfie_final_exp_202303/p2/1_1/2023-03-20_23-38-09')  # tuning curve
-    # directory = Path('G:/Aryo/copy_data/Elfie_final_exp_202303/p1/15/2023-03-20_21-42-51')  # music data
+def main():
     
-    # SERVER
-    # get experimetn path whose trigger you want to extract
-    # directory = '/home/ssenapat/groups/PrimNeu/Aryo/copy_data/Elfie_final_exp_202303/p2/1_1/2023-03-20_23-38-09'
-    # directory = '/home/ssenapat/groups/PrimNeu/Aryo/copy_data/Elfie_final_exp_202303/p1/1/2023-03-20_15-38-11'
+    all_exps = get_inputs() 
     
     
-    session = Session(directory)
-    
-    recording = session.recordnodes[0].recordings[0]
-    
-    # Extract trigger channel - not required to run after saving the trigger channel
-    
-    ap = recording.continuous[0].samples
-    ap_trigger = ap[:, 384]
-    
-    lfp = recording.continuous[1].samples
-    lfp_trigger = lfp[:, 384]
-    
-    
-    
-    
-    ## uncomment based on requirement
-    save_AP_trigger_MU(ap_trigger, directory)
-    
-    save_AP_trigger_SU(ap_trigger, directory)
-    
-    save_LFP_trigger(lfp_trigger, directory)
-    
-    # trg = pd.DataFrame(music_trigger)
-   
+    for i in range(0,all_exps.size):
+        
+        if all_exps.size == 1:
+            directory = Path(all_exps.tolist())     # when working with single path in destinations.txt file
+        else:
+            directory = Path(all_exps[i])
+        
+        print(directory)
+        session = Session(directory)
+        recording = session.recordnodes[0].recordings[0]
+        
+        
+        # Extract trigger channel - not required to run after saving the trigger channel
+        
+        ap = recording.continuous[0].samples
+        ap_trigger = ap[:, 384]
+        
+        lfp = recording.continuous[1].samples
+        lfp_trigger = lfp[:, 384]
+        
+        
+        ## uncomment based on requirement
+        save_AP_trigger_MU(ap_trigger, directory)
+        
+        # save_AP_trigger_SU(ap_trigger, directory)
+        
+        #save_LFP_trigger(lfp_trigger, directory)
+        
+        # trg = pd.DataFrame(music_trigger)
+        
+       
 
 
 
 from open_ephys.analysis import Session
 from pathlib import Path
 import numpy as np
-import pandas as pd
 import os
 import re
 
